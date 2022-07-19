2cf977849076fb8a967a97c190bd201f8cad9441
Frederick Mejia <fjdevel@gmail.com>
2022-07-19 17:23:23 -0600

    first commit

---
+++ src/main/java/com/piratemarket/piratemarket/PiratemarketApplication.java
@@ -0,0 +1,13 @@
+package com.piratemarket.piratemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiratemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiratemarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piratemarket/piratemarket/PiratemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piratemarket.piratemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiratemarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
